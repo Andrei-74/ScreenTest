cfd8b8e013f93b41b0218ce8e3103d1b41249572
Andrei <registr740@yandex.ru>
2022-02-23 03:37:16 +0500

    Initial Commit

---
+++ ScreenTest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ScreenTest
+//
+//  Created by Андрей Костицин on 23.02.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+var screen = ""
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ScreenTest/ScreenTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScreenTestApp.swift
+//  ScreenTest
+//
+//  Created by Андрей Костицин on 23.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct ScreenTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
